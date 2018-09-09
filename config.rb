05171c97b9e3fd5966e03094fd305d63fde00cac
PikachuHy <pikachuhy@163.com>
2018-09-09 13:10:15 +0800

    use csn

--- config.rb
+++ config.rb
@@ -39,10 +39,10 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
 
 #activate :relative_assets
